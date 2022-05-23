16d31ffc511d4ebdc79d0648fa7bd2462022ec17
Kirill Kuznetsov <kuznechik.86@mail.ru>
2022-05-23 21:00:20 +0300

    Initial commit

---
+++ src/main/java/ru/kuznetsovka/rightStepTest/RightStepTestApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsovka.rightStepTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightStepTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightStepTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kuznetsovka/rightStepTest/RightStepTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsovka.rightStepTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightStepTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
